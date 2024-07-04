ab392b4d0121be0fccadd4611f89c64ae78cad25
mHaruo <141676006+mHaruo@users.noreply.github.com>
2024-07-04 17:14:16 -0300

    ADD: Desafio registro de transações com Stream API

---
+++ RegistroComStreamAPI/src/RegistroTransacoesComStream.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RegistroTransacoesComStream {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        int quantidadeTransacoes = scanner.nextInt();
+
+        List<Transacao> transacoes = new ArrayList<>();
+
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+            char tipoTransacao = scanner.next().toUpperCase().charAt(0);
+            double valorTransacao = scanner.nextDouble();
+
+            Transacao transacao = new Transacao(tipoTransacao, valorTransacao);
+            transacoes.add(transacao);
+
+            if (transacao.getTipo() == 'D') {
+                saldo += valorTransacao;
+            } else if (transacao.getTipo() == 'S') {
+                saldo -= valorTransacao;
+            }
+        }
+
+        System.out.println("Saldo : " + saldo);
+        System.out.println("Transacoes:");
+        transacoes.stream()
+                .map(transacao ->
+                        (transacao.getTipo() == 'D' ? "d de " : "s de ") + transacao.getValor())
+                .toList()
+                .forEach(System.out::println);
+
+        scanner.close();
+    }
+}
+
+class Transacao {
+    private char tipo;
+    private double valor;
+
+    public Transacao(char tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
